99c2c8501d38c16367f1966cf5648051cfc59f2b
João Victor <joaovictor@delotech.com.br>
2021-02-16 15:34:29 -0300

    Iniciando o game

---
+++ Assets/Movefundo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movefundo : MonoBehaviour
+{
+
+    float larguraImagem;
+    float alturaImagem;
+    float alturaTela;
+    float larguraTela;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        SpriteRenderer imagem = GetComponent<SpriteRenderer>();
+
+        larguraImagem = imagem.sprite.bounds.size.x;
+        alturaImagem = imagem.sprite.bounds.size.y;
+
+        alturaTela = Camera.main.orthographicSize * 2f;
+        //Debug.LogError(alturaTela);
+
+        larguraTela = alturaTela / Screen.height * Screen.width;
+        //Debug.LogError(larguraTela);
+
+        Vector2 novaEscala = transform.localScale;
+        novaEscala.x = larguraTela / larguraImagem + 0.25f;
+        novaEscala.y = alturaTela / alturaImagem;
+        transform.localScale = novaEscala;
+
+        if(this.name == "imagemFundoB")
+        {
+            transform.position = new Vector2(larguraTela, 0f);
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-3, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x <= -larguraTela)
+        {
+            transform.position = new Vector2(larguraTela, 0f);
+        }
+    }
+}
